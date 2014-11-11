9591a7e82ad642adfeb0baa31c3efe32bd617e2f
zathras-crypto <zathrasc@gmail.com>
2014-11-10 17:10:33 -0800

    initials for orderListDelegate

---
+++ src/qt/orderlistdelegate.h
@@ -0,0 +1,14 @@
+#include <QPainter>
+#include <QAbstractItemDelegate>
+
+class ListDelegate : public QAbstractItemDelegate
+{
+	public:
+		ListDelegate(QObject *parent = 0);
+	
+		void paint ( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const;
+		QSize sizeHint ( const QStyleOptionViewItem & option, const QModelIndex & index ) const;
+		
+		virtual ~ListDelegate();
+	
+};
