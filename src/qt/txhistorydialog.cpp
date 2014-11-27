54380448b91ff11c3fffd7865659795207d98450
zathras-crypto <zathrasc@gmail.com>
2014-11-27 15:13:48 -0800

    Add UI elements for tx history

---
+++ src/qt/txhistorydialog.cpp
@@ -0,0 +1,283 @@
+// Copyright (c) 2011-2014 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "txhistorydialog.h"
+#include "ui_txhistorydialog.h"
+
+#include "guiutil.h"
+#include "optionsmodel.h"
+#include "walletmodel.h"
+#include "wallet.h"
+#include "base58.h"
+#include "ui_interface.h"
+
+#include <boost/filesystem.hpp>
+
+#include "leveldb/db.h"
+#include "leveldb/write_batch.h"
+
+// potentially overzealous includes here
+#include "base58.h"
+#include "rpcserver.h"
+#include "init.h"
+#include "util.h"
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <string>
+#include <boost/assign/list_of.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/find.hpp>
+#include <boost/algorithm/string/join.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/format.hpp>
+#include <boost/filesystem.hpp>
+#include "json/json_spirit_utils.h"
+#include "json/json_spirit_value.h"
+#include "leveldb/db.h"
+#include "leveldb/write_batch.h"
+// end potentially overzealous includes
+
+using namespace json_spirit;
+#include "mastercore.h"
+using namespace mastercore;
+
+// potentially overzealous using here
+using namespace std;
+using namespace boost;
+using namespace boost::assign;
+using namespace leveldb;
+// end potentially overzealous using
+
+#include "mastercore_dex.h"
+#include "mastercore_tx.h"
+#include "mastercore_sp.h"
+#include "mastercore_parse_string.h"
+
+#include <QDateTime>
+#include <QMessageBox>
+#include <QScrollBar>
+#include <QTextDocument>
+
+#include "txlistdelegate.h"
+
+TXHistoryDialog::TXHistoryDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::txHistoryDialog),
+    model(0)
+{
+    ui->setupUi(this);
+    this->model = model;
+    ui->txHistoryLW->setItemDelegate(new ListDelegate(ui->txHistoryLW));
+
+    CWallet *wallet = pwalletMain;
+    string sAddress = "";
+    string addressParam = "";
+    bool addressFilter;
+
+    addressFilter = false;
+    int64_t nCount = 10;
+    int64_t nFrom = 0;
+    int64_t nStartBlock = 0;
+    int64_t nEndBlock = 999999;
+
+    Array response; //prep an array to hold our output
+
+    // rewrite to use original listtransactions methodology from core
+    LOCK(wallet->cs_wallet);
+    std::list<CAccountingEntry> acentries;
+    CWallet::TxItems txOrdered = pwalletMain->OrderedTxItems(acentries, "*");
+
+    // iterate backwards 
+    for (CWallet::TxItems::reverse_iterator it = txOrdered.rbegin(); it != txOrdered.rend(); ++it)
+    {
+        CWalletTx *const pwtx = (*it).second.first;
+        if (pwtx != 0)
+        {
+            uint256 hash = pwtx->GetHash();
+            CTransaction wtx;
+            uint256 blockHash = 0;
+            if (!GetTransaction(hash, wtx, blockHash, true)) continue;
+            // get the time of the tx
+            int64_t nTime = pwtx->GetTxTime();
+            // get the height of the transaction and check it's within the chosen parameters
+            blockHash = pwtx->hashBlock;
+            if ((0 == blockHash) || (NULL == mapBlockIndex[blockHash])) continue;
+            CBlockIndex* pBlockIndex = mapBlockIndex[blockHash];
+            if (NULL == pBlockIndex) continue;
+            int blockHeight = pBlockIndex->nHeight;
+            if ((blockHeight < nStartBlock) || (blockHeight > nEndBlock)) continue; // ignore it if not within our range
+            // check if the transaction exists in txlist, and if so is it correct type (21)
+            if (p_txlistdb->exists(hash))
+            {
+                // get type from levelDB
+                string strValue;
+                if (!p_txlistdb->getTX(hash, strValue)) continue;
+                std::vector<std::string> vstr;
+                boost::split(vstr, strValue, boost::is_any_of(":"), token_compress_on);
+                if (4 <= vstr.size())
+                {
+                    // if tx21, get the details for the list
+                    if(21 == atoi(vstr[2]))
+                    {
+                        string statusText;
+                        unsigned int propertyIdForSale = 0;
+                        unsigned int propertyIdDesired = 0;
+                        uint64_t amountForSale = 0;
+                        uint64_t amountDesired = 0;
+                        string address;
+                        bool divisibleForSale;
+                        bool divisibleDesired;
+                        bool valid;
+                        Array tradeArray;
+                        uint64_t totalBought = 0;
+                        uint64_t totalSold = 0;
+                        bool orderOpen = false;
+
+                        CMPMetaDEx temp_metadexoffer;
+                        CMPTransaction mp_obj;
+                        int parseRC = parseTransaction(true, wtx, blockHeight, 0, &mp_obj);
+                        if (0 <= parseRC) //negative RC means no MP content/badly encoded TX, we shouldn't see this if TX in levelDB but check for sanity
+                        {
+                            if (0<=mp_obj.step1())
+                            {
+                                //MPTxType = mp_obj.getTypeString();
+                                //MPTxTypeInt = mp_obj.getType();
+                                address = mp_obj.getSender();
+                                //if (!filterAddress.empty()) if ((senderAddress != filterAddress) && (refAddress != filterAddress)) return -1; // return negative rc if filtering & no match
+
+                                int tmpblock=0;
+                                uint32_t tmptype=0;
+                                uint64_t amountNew=0;
+                                valid=getValidMPTX(hash, &tmpblock, &tmptype, &amountNew);
+
+                                if (0 == mp_obj.step2_Value())
+                                {
+                                    propertyIdForSale = mp_obj.getProperty();
+                                    amountForSale = mp_obj.getAmount();
+                                    divisibleForSale = isPropertyDivisible(propertyIdForSale);
+                                    if (0 <= mp_obj.interpretPacket(NULL,&temp_metadexoffer))
+                                    {
+                                        propertyIdDesired = temp_metadexoffer.getDesProperty();
+                                        divisibleDesired = isPropertyDivisible(propertyIdDesired);
+                                        amountDesired = temp_metadexoffer.getAmountDesired();
+                                        //mdex_action = temp_metadexoffer.getAction();
+                                        t_tradelistdb->getMatchingTrades(hash, propertyIdForSale, &tradeArray, &totalSold, &totalBought);
+
+                                        // status - is order cancelled/closed-filled/open/open-partialfilled?
+                                        // is the sell offer still open - need more efficient way to do this
+                                        for (md_PropertiesMap::iterator my_it = metadex.begin(); my_it != metadex.end(); ++my_it)
+                                        {
+                                            if (my_it->first == propertyIdForSale) //at minimum only go deeper if it's the right property id
+                                            {
+                                                md_PricesMap & prices = my_it->second;
+                                                for (md_PricesMap::iterator it = prices.begin(); it != prices.end(); ++it)
+                                                {
+                                                    md_Set & indexes = (it->second);
+                                                    for (md_Set::iterator it = indexes.begin(); it != indexes.end(); ++it)
+                                                    {
+                                                        CMPMetaDEx obj = *it;
+                                                        if( obj.getHash().GetHex() == hash.GetHex() ) orderOpen = true;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        // work out status
+                        bool partialFilled = false;
+                        bool filled = false;
+                        if(totalSold>0) partialFilled = true;
+                        if(totalSold>=amountForSale) filled = true;
+                        statusText = "UNKNOWN";
+                        if((!orderOpen) && (!partialFilled)) statusText = "CANCELLED";
+                        if((!orderOpen) && (partialFilled)) statusText = "PART CANCEL";
+                        if((!orderOpen) && (filled)) statusText = "FILLED";
+                        if((orderOpen) && (!partialFilled)) statusText = "OPEN";
+                        if((orderOpen) && (partialFilled)) statusText = "PART FILLED";
+
+                        // add to list
+                        QListWidgetItem *qItem = new QListWidgetItem();
+                        qItem->setData(Qt::DisplayRole, QString::fromStdString(hash.GetHex()));
+                        string displayText = "Sell ";
+                        string displayIn = "+";
+                        string displayOut = "-";
+                        string displayInToken;
+                        string displayOutToken;
+
+                        if(divisibleForSale) { displayText += FormatDivisibleMP(amountForSale); } else { displayText += FormatIndivisibleMP(amountForSale); }
+                        if(propertyIdForSale < 3)
+                        {
+                            if(propertyIdForSale == 1) { displayText += " MSC for "; displayOutToken = " MSC"; }
+                            if(propertyIdForSale == 2) { displayText += " TMSC for "; displayOutToken = " TMSC"; }
+                        }
+                        else
+                        {
+                            string s = to_string(propertyIdForSale);
+                            displayText += " SPT#" + s + " for ";
+                            displayOutToken = " SPT#" + s;
+                        }
+                        if(divisibleDesired) { displayText += FormatDivisibleMP(amountDesired); } else { displayText += FormatIndivisibleMP(amountDesired); }
+                        if(propertyIdDesired < 3)
+                        {
+                            if(propertyIdDesired == 1) { displayText += " MSC"; displayInToken = " MSC"; }
+                            if(propertyIdDesired == 2) { displayText += " TMSC"; displayInToken = " TMSC"; }
+                        }
+                        else
+                        {
+                            string s = to_string(propertyIdDesired);
+                            displayText += " SPT#" + s;
+                            displayInToken = " SPT#" + s;
+                        }
+                        if(divisibleDesired) { displayIn += FormatDivisibleMP(totalBought); } else { displayIn += FormatIndivisibleMP(totalBought); }
+                        if(divisibleForSale) { displayOut += FormatDivisibleMP(totalSold); } else { displayOut += FormatIndivisibleMP(totalSold); }
+                        if(totalBought == 0) displayIn = "0";
+                        if(totalSold == 0) displayOut = "0";
+                        displayIn += displayInToken;
+                        displayOut += displayOutToken;
+                        QDateTime txTime;
+                        txTime.setTime_t(nTime);
+                        QString txTimeStr = txTime.toString(Qt::SystemLocaleShortDate);
+                        qItem->setData(Qt::UserRole + 1, QString::fromStdString(displayText));
+                        qItem->setData(Qt::UserRole + 2, QString::fromStdString(displayIn));
+                        qItem->setData(Qt::UserRole + 3, QString::fromStdString(displayOut));
+                        qItem->setData(Qt::UserRole + 4, QString::fromStdString(statusText));
+                        qItem->setData(Qt::UserRole + 5, QString::fromStdString(address));
+                        qItem->setData(Qt::UserRole + 6, txTimeStr);
+                        ui->txHistoryLW->addItem(qItem);
+                    }
+                }
+            }
+            // don't burn time doing more work than we need to
+//            if ((int)response.size() >= (nCount+nFrom)) break;
+        }
+    }
+    // sort array here and cut on nFrom and nCount
+//    if (nFrom > (int)response.size())
+//        nFrom = response.size();
+//    if ((nFrom + nCount) > (int)response.size())
+//        nCount = response.size() - nFrom;
+//    Array::iterator first = response.begin();
+//    std::advance(first, nFrom);
+//    Array::iterator last = response.begin();
+//    std::advance(last, nFrom+nCount);
+
+//    if (last != response.end()) response.erase(last, response.end());
+//    if (first != response.begin()) response.erase(response.begin(), first);
+
+//    std::reverse(response.begin(), response.end()); // return oldest to newest?
+ //   return response;   // return response array for JSON serialization
+
+}
+
+void TXHistoryDialog::setModel(WalletModel *model)
+{
+    this->model = model;
+    //connect(model, SIGNAL(balanceChanged(qint64, qint64, qint64)), this, SLOT(OrderRefresh()));
+}
+
---
+++ src/qt/txlistdelegate.cpp
@@ -0,0 +1,106 @@
+#include "txlistdelegate.h"
+
+ListDelegate::ListDelegate(QObject *parent)
+{
+
+}
+
+void ListDelegate::paint ( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const
+{
+    QRect r = option.rect;
+    QPen linePen(QColor::fromRgb(211,211,211), 1, Qt::SolidLine);
+    QPen fontPen(QColor::fromRgb(51,51,51), 1, Qt::SolidLine);
+    painter->setPen(linePen);
+
+    // alt the colors
+    painter->setBrush( (index.row() % 2) ? Qt::white : QColor(252,252,252) );
+    painter->drawRect(r);
+    // draw border
+    painter->setPen(linePen);
+    painter->drawLine(r.topLeft(),r.topRight());
+    painter->drawLine(r.topRight(),r.bottomRight());
+    painter->drawLine(r.bottomLeft(),r.bottomRight());
+    painter->drawLine(r.topLeft(),r.bottomLeft());
+    painter->setPen(fontPen);
+
+    // prepare the data for the entry
+// QIcon ic = QIcon(qvariant_cast<QPixmap>(index.data(Qt::DecorationRole)));
+//    string shortTXID = QString::fromStdString(index.data(Qt::DisplayRole).toString().toStdString().substr(0,12));
+    QString txidsender = "ADD: " + QString::fromStdString(index.data(Qt::UserRole + 5).toString().toStdString().substr(0,18)) + "...";
+    txidsender += "   TX: " + QString::fromStdString(index.data(Qt::DisplayRole).toString().toStdString().substr(0,18)) + "...";
+//    txidstatus += "....\tSTATUS: " + index.data(Qt::UserRole + 4).toString();
+    QString displayText = index.data(Qt::UserRole + 1).toString();
+    QString amountBought = index.data(Qt::UserRole + 2).toString();
+    QString amountSold = index.data(Qt::UserRole + 3).toString();
+    QString status = index.data(Qt::UserRole + 4).toString();
+    QString senderText = index.data(Qt::UserRole + 5).toString();
+    QString txTimeText = index.data(Qt::UserRole + 6).toString();
+
+    // add the appropriate status icon
+    int imageSpace = 10;
+    QIcon ic = QIcon(":/icons/meta_cancelled");
+    if(status == "CANCELLED") ic =QIcon(":/icons/meta_cancelled");
+    if(status == "PART CANCEL") ic = QIcon(":/icons/meta_partialclosed");
+    if(status == "FILLED") ic = QIcon(":/icons/meta_filled");
+    if(status == "OPEN") ic = QIcon(":/icons/meta_open");
+    if(status == "PART FILLED") ic = QIcon(":/icons/meta_partial");
+    if (!ic.isNull())
+    {
+        r = option.rect.adjusted(5, 10, -10, -10);
+        ic.paint(painter, r, Qt::AlignVCenter|Qt::AlignLeft);
+        imageSpace = 60;
+    }
+
+    // setup pens
+    QPen penBlack(QColor("#000000"));
+    QPen penRed(QColor("#CC0000"));
+    QPen penGreen(QColor("#00AA00"));
+    QPen penGrey(QColor("#606060"));
+
+    QFont font = painter->font();
+    painter->setPen(penBlack);
+    // add the status
+    font.setItalic(false);
+    painter->setFont(font);
+    r = option.rect.adjusted(imageSpace-19, 0, -10, -25);
+    painter->drawText(r.left(), r.top(), r.width(), r.height(), Qt::AlignBottom|Qt::AlignLeft, status, &r);
+    // add the datetime
+    painter->setPen(penGrey);
+    font.setItalic(true);
+    painter->setFont(font);
+    r = option.rect.adjusted(imageSpace-19, 25, -10, 0);
+    painter->drawText(r.left(), r.top(), r.width(), r.height(), Qt::AlignLeft, txTimeText, &r);
+    // add the displaytext
+    painter->setPen(penBlack);
+    r = option.rect.adjusted(imageSpace+115, 0, -10, -25);
+    font.setBold(true);
+    font.setItalic(false);
+    painter->setFont(font);
+    painter->drawText(r.left(), r.top(), r.width(), r.height(), Qt::AlignBottom|Qt::AlignLeft, displayText, &r);
+    // add the txid/sender
+    painter->setPen(penGrey);
+    font.setBold(false);
+    painter->setFont(font);
+    r = option.rect.adjusted(imageSpace+115, 25, -10, 0);
+    painter->drawText(r.left(), r.top(), r.width(), r.height(), Qt::AlignLeft, txidsender, &r);
+    font.setBold(true);
+    painter->setFont(font);
+    if("0 " != amountBought.toStdString().substr(0,2)) painter->setPen(penGreen);
+    r = option.rect.adjusted(imageSpace+115, 0, -10, -25);
+    painter->drawText(r.left(), r.top(), r.width(), r.height(), Qt::AlignBottom|Qt::AlignRight, amountBought, &r);
+    if("0 " != amountSold.toStdString().substr(0,2)) painter->setPen(penRed);
+    r = option.rect.adjusted(imageSpace+115, 25, -10, 0);
+    painter->drawText(r.left(), r.top(), r.width(), r.height(), Qt::AlignRight, amountSold, &r);
+    font.setBold(false);
+    painter->setFont(font);
+
+}
+
+QSize ListDelegate::sizeHint ( const QStyleOptionViewItem & option, const QModelIndex & index ) const
+{
+    return QSize(200, 50); // very dumb value?
+}
+
+ListDelegate::~ListDelegate()
+{
+}
