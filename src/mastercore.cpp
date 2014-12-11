06fa7c12ca57c1eb9afacd4a33eda803a830a481
Michael <michael@mastercoin.org>
2014-12-11 15:04:50 -0500

    the fix :)

--- src/mastercore.cpp
+++ src/mastercore.cpp
@@ -692,8 +692,6 @@ uint64_t before, after;
     return false;
   }
 
-  if (string("1zAtHRASgdHvZDfHs6xJquMghga4eG7gy") == who) ++amount; // FIXME
-
   LOCK(cs_tally);
 
   before = getMPbalance(who, which_property, ttype);
