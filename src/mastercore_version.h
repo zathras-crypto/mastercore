6a3d2008aa5293b0a9d602ddf3141aba67ca4b15
zathras-crypto <zathrasc@gmail.com>
2014-12-18 13:51:09 -0800

    Commit and track missing new files

---
+++ src/mastercore_version.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2013 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef _MASTERCORE_VERSION_H
+#define _MASTERCORE_VERSION_H
+
+  #define OMNICORE_VERSION_BASE 90 // 82 = 0.0.8.2   91 = 0.0.9.1   103 = 0.0.10.3 etc
+  #define OMNICORE_VERSION_TYPE "-dev" // switch to -rel for tags, switch back to -dev for development
+
+  /* use this when we tag 0.0.9
+  #define OMNICORE_VERSION_BASE 90
+  #define OMNICORE_VERSION_TYPE "-rel"
+  */
+
+  /* after tagging switch it onto the next dev version
+  #define OMNICORE_VERSION_BASE 91
+  #define OMNICORE_VERSION_TYPE "-dev"
+  */
+
+#endif
