e38201a152aca26e473113d59d67fee6c5b89aee
zathras-crypto <zathrasc@gmail.com>
2015-02-01 19:55:17 -0800

    Fix version for tag

--- src/mastercore_version.h
+++ src/mastercore_version.h
@@ -6,12 +6,7 @@
 #define _MASTERCORE_VERSION_H
 
   #define OMNICORE_VERSION_BASE 90 // 82 = 0.0.8.2   91 = 0.0.9.1   103 = 0.0.10.3 etc
-  #define OMNICORE_VERSION_TYPE "-dev" // switch to -rel for tags, switch back to -dev for development
-
-  /* use this when we tag 0.0.9
-  #define OMNICORE_VERSION_BASE 90
-  #define OMNICORE_VERSION_TYPE "-rel"
-  */
+  #define OMNICORE_VERSION_TYPE "-rel" // switch to -rel for tags, switch back to -dev for development
 
   /* after tagging switch it onto the next dev version
   #define OMNICORE_VERSION_BASE 91
