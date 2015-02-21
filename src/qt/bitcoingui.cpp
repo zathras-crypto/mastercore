97232e867f19ad2e21dcd62e589647860e197aa1
dexX7 <dexx@bitwatch.co>
2015-02-21 23:04:33 +0100

    QT: use "Omni Core" as default message box title
    This affects ThreadSafeMessageBox, InitError, InitWarning ...

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -800,7 +800,7 @@ void BitcoinGUI::setNumBlocks(int count)
 
 void BitcoinGUI::message(const QString &title, const QString &message, unsigned int style, bool *ret)
 {
-    QString strTitle = tr("Bitcoin"); // default title
+    QString strTitle = tr("Omni Core"); // default title
     // Default to information icon
     int nMBoxIcon = QMessageBox::Information;
     int nNotifyIcon = Notificator::Information;
