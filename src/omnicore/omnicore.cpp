a984721ee3925d43d924ec56bed7fd49b6ba9c84
dexX7 <dexx@bitwatch.co>
2016-10-07 22:50:51 +0200

    Add dummy handler

--- src/omnicore/omnicore.cpp
+++ src/omnicore/omnicore.cpp
@@ -8,6 +8,9 @@
 
 #include "omnicore/log.h"
 
+#include "chain.h"
+#include "primitives/transaction.h"
+
 
 /**
  * Global handler to initialize Omni Core.
@@ -42,3 +45,40 @@ void CheckWalletUpdate(bool forceUpdate)
 {
     // TODO
 }
+
+int mastercore_handler_block_begin(int nBlockPrev, CBlockIndex const * pBlockIndex)
+{
+    // TODO
+    return 0;
+}
+
+int mastercore_handler_block_end(int nBlockNow, CBlockIndex const * pBlockIndex,
+        unsigned int countMP)
+{
+    // TODO
+    return 0;
+}
+
+int mastercore_handler_disc_begin(int nBlockNow, CBlockIndex const * pBlockIndex)
+{
+    // TODO
+    return 0;
+}
+
+int mastercore_handler_disc_end(int nBlockNow, CBlockIndex const * pBlockIndex)
+{
+    // TODO
+    return 0;
+}
+
+/**
+ * This handler is called for every new transaction that comes in (actually in block parsing loop).
+ *
+ * @return True, if the transaction was an Exodus purchase, DEx payment or a valid Omni transaction
+ */
+int mastercore_handler_tx(const CTransaction& tx, int nBlock, unsigned int idx, const CBlockIndex* pBlockIndex)
+{
+    // TODO
+    return 0;
+}
+
--- src/omnicore/omnicore.h
+++ src/omnicore/omnicore.h
@@ -1,6 +1,9 @@
 #ifndef OMNICORE_OMNICORE_H
 #define OMNICORE_OMNICORE_H
 
+class CBlockIndex;
+class CTransaction;
+
 /** Global handler to initialize Omni Core. */
 int mastercore_init();
 
@@ -9,6 +12,12 @@ int mastercore_shutdown();
 
 /** Global handler to total wallet balances. */
 void CheckWalletUpdate(bool forceUpdate = false);
+
+int mastercore_handler_disc_begin(int nBlockNow, CBlockIndex const * pBlockIndex);
+int mastercore_handler_disc_end(int nBlockNow, CBlockIndex const * pBlockIndex);
+int mastercore_handler_block_begin(int nBlockNow, CBlockIndex const * pBlockIndex);
+int mastercore_handler_block_end(int nBlockNow, CBlockIndex const * pBlockIndex, unsigned int countMP);
+int mastercore_handler_tx(const CTransaction &tx, int nBlock, unsigned int idx, CBlockIndex const * pBlockIndex);
 
 
 #endif // OMNICORE_OMNICORE_H
