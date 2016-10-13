83c238b250c9fa2e7d91e162dbcc2dbedb20672c
dexX7 <dexx@bitwatch.co>
2016-10-13 23:57:02 +0200

    Add data retrieval RPCs

---
+++ src/omnicore/rpc.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   rpc.h
+ * Author: dexx
+ *
+ * Created on 13. Oktober 2016, 23:32
+ */
+
+#ifndef RPC_H
+#define RPC_H
+
+
+
+#endif /* RPC_H */
+
