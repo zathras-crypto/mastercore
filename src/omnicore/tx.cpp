27fa5d2181a60d8180c0248a62f60b9fbee182d3
Zathras <zathrasc@gmail.com>
2017-12-10 15:41:26 +1100

    Verify the Bitcoin address extracted from the payload is valid

--- src/omnicore/tx.cpp
+++ src/omnicore/tx.cpp
@@ -17,6 +17,7 @@
 #include "omnicore/utils.h"
 
 #include "amount.h"
+#include "base58.h"
 #include "main.h"
 #include "sync.h"
 #include "utiltime.h"
@@ -706,6 +707,10 @@ bool CMPTransaction::interpret_FreezeTokens()
     if (receiver.empty()) {
         return false;
     }
+    CBitcoinAddress recAddress(receiver);
+    if (!recAddress.IsValid()) {
+        return false;
+    }
 
     if ((!rpcOnly && msc_debug_packets) || msc_debug_packets_readonly) {
         PrintToLog("\t        property: %d (%s)\n", property, strMPProperty(property));
@@ -739,6 +744,10 @@ bool CMPTransaction::interpret_UnfreezeTokens()
     memcpy(&address_hash160, &pkt[17], 20);
     receiver = HashToAddress(address_version, address_hash160);
     if (receiver.empty()) {
+        return false;
+    }
+    CBitcoinAddress recAddress(receiver);
+    if (!recAddress.IsValid()) {
         return false;
     }
 
